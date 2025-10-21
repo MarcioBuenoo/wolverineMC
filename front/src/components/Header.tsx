930b609c7166006cbd77ce3917dc39bbba696445
MarcioBuenoo <marcinbueno01@hotmail.com>
2025-10-20 21:36:52 -0300

    commitIncial_20-10

---
+++ front/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import '../styles/header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <a href="">Sobre nós</a>
+            <a href="">Missão</a>
+            <a href="">Visão</a>
+            <a href="">Valores</a>
+        </header>
+    )
+}
