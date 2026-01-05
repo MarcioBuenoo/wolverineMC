5861ff08ac85a4c8ac8fb5a14c3265aaecbcdc5b
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-05 18:59:17 -0300

    adição missão_05-01_19:00

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,14 @@
 import './styles/app.css'
 import Header from './components/Header'
 import Introducao from './components/Introduçao'
+import Missao from './components/Missao'
 
 function App() {
   return (
     <div>
       <Header />
       <Introducao />
+      <Missao />
     </div>
   )
 }
---
+++ front/src/components/Missao.tsx
@@ -0,0 +1,17 @@
+import '../styles/missao.css'
+
+export default function Missao() {
+    return (
+        <div className="container-missao">
+            <div className='titulo'>
+                <h2>Missão</h2>
+            </div>
+            <div className='texto'>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever <br /> 
+                since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only <br />
+                five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release <br />
+                of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
+        </div>
+    )
+}
