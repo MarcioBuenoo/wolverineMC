87b58cf6ceaa40a931d46f7f6a677aef7cbee5b2
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-06 19:30:40 -0300

    adição valores_06-01_19:30

---
+++ front/src/components/Valores.tsx
@@ -0,0 +1,11 @@
+export default function Valores() {
+    return (
+        <div className="container-valores">
+            <div className="titulo-valores">
+                <h2>Valores</h2>
+            </div>
+            <div className="modals-valores">
+            </div>
+        </div>
+    )
+}
