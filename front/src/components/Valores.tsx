0c504558c5ca04046ff91d5cc4940477d1cd7322
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-07 19:28:22 -0300

    atualização do segmento valores_07-01_19:28

--- front/src/components/Valores.tsx
+++ front/src/components/Valores.tsx
@@ -9,6 +9,9 @@ export default function Valores() {
             </div>
             <div className="modals-valores">
                 <Card titulo={'Respeito'} conteudo={'Tratamos todos com dignidade, valorizando a diversidade e promovendo um ambiente inclusivo.'} />
+                <Card titulo={'Irmandade'} conteudo={'Agimos com honestidade e ética em todas as nossas interações, somos uma família.'} />
+                <Card titulo={'Liberdade'} conteudo={'Prezamos pelos direitos individuais, ser livre.'} />
+                <Card titulo={`Solidariedade`} conteudo={'Apoiamos uns aos outros, promovendo um ambiente de cooperação e ajuda mútua.'} />
             </div>
         </div>
     )
