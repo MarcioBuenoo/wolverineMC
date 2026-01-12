b7ebfd8b3b6980f8d160e647fe1559423dcd4162
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-12 20:46:55 -0300

    Retrabalho no Header e Introducao_12-01_20:46

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -4,6 +4,10 @@ export default function Header() {
     return (
         <nav className='header-container'>
             <img className='header-imagem' src="/wolverineMC.png" alt="Logo do M.C" />
+            <a className='header-buttons' href="">Missão</a>
+            <a className='header-buttons' href="">Visão</a>
+            <a className='header-buttons' href="">Valores</a>
+            <a className='header-buttons-2nd' href="">Entre em Contato</a>
         </nav>
     )
 }
