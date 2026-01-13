cc788f804aa4bfa6a49807fa103bc33de9087c08
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-12 21:21:25 -0300

    Retrabalho Missao_12-01_21:21

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,14 @@
 import './styles/app.css'
 import Header from './components/Header'
 import Introducao from './components/Introduçao'
+import Missao from './components/Missao'
 
 function App() {
   return (
     <div className='index-div'>
       <Header />
       <Introducao />
+      <Missao />
     </div>
   )
 }
--- front/src/components/Missao.tsx
+++ front/src/components/Missao.tsx
@@ -3,10 +3,10 @@ import '../styles/missao.css'
 export default function Missao() {
     return (
         <div className="container-missao">
-            <div className='titulo-missao'>
+            <span className='titulo-missao'>
                 <h2 className='titulo-missao-h2'>Missão</h2>
-            </div>
-            <div className='texto-missao'>
+            </span>
+            <span className='texto-missao'>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever 
                     since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only 
@@ -14,7 +14,7 @@ export default function Missao() {
                     of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
                 <img className='foto-missao' src="../../public/fotoG.jpeg" alt="Foto do grupo" />
-            </div>
+            </span>
         </div>
     )
 }
